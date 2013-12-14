63539a941dc8b7656d7515f40a6afeda67967abe
KianJones <kjones43@u.rochester.edu>
2013-12-14 00:41:29 -0500

    added some docs

--- PokeCalc.py
+++ PokeCalc.py
@@ -6,14 +6,24 @@ from dex import dex, nameList, nature
 def quit():
     print 'error'; exit(0)
 
-order ={1:'HP', 2:'Attack', 3:'Defense', 4:'Special Attack', 
-                5:'Special Defense', 6:'Speed'}
-stats = "HP, Attack, Defense, Speed, Special Attack, Special Defense"
 class Pokemon(object):
-    def __init__(self, name, level, nature, statsL = [], iv = [], ev = [0,0,0,0,0,0]):
+    """ The Pokemon class that you use the pokemon methods with.
+        
+        Instance variables:
+        String name -- the actual name of the pokemon, lowercase
+        int level -- the level it is at, or the level you want to test
+        String nature -- nature in all lowercase
+        statsL -- the list of stats[HP,Atk,Def,SpA,SpD,Spe]
+        iv -- the list of ivs, same order
+        ev -- the list of evs, same order
+       """
+    def __init__(self, name, level, nature, statsL=[], iv=[], ev=[]):
+        # I can split this on , so that I don't have to type it repeatedly
         stats = "HP, Attack, Defense, Speed, Special Attack, Special Defense"
+        # Makes sure that stats are always put in in this order
         order ={1:'HP', 2:'Attack', 3:'Defense', 4:'Special Attack', 
                 5:'Special Defense', 6:'Speed'}
+                
         self.name = name
         self.level = level
         self.stats = {stat: None for stat in stats.split(', ')}
@@ -22,6 +32,7 @@ class Pokemon(object):
         self.evL = ev
         self.nature = nature
         
+        # checks if the list of stats/ev/iv was given 
         if len(statsL) > 0:
             for i in range(1,7):
                 self.stats[order[i]] = statsL[i-1]
@@ -33,45 +44,51 @@ class Pokemon(object):
         if len(ev) > 0:
             for i in range(1,7):
                 self.ev[order[i]] = ev[i-1]
-                
-                
+        else:
+            for i in range(1,7):
+                self.ev[i] = 0
                 
 			
 def stat_calc(stat, base, ev, level):
+    """ Returns the calculated non-HP stat at given level. """
     num = (int(stat) + (2*base) + ev/4.0 )*int(level)
     result = (num/100.0) + 5
     return result
 	
 def hpstat_calc(hp, base, ev, level):
+    """ Returns the calculated HP stat at given level. """
     num = ((int(hp) + (2*base) + ev/4.0 + 100) * int(level))
     result = (num/100.0) + 10
     return result
     
 def iv_calc(stat, base, ev, level):
+    """ Returns the calculated non-HP IV. """
     a = (100*(stat-5))/level
     b = -(2*base)-(ev/4)
     return a+b+1
     
 def hpiv_calc(hp, base, ev, level):
+    """ Returns the calculated HP IV. """
     a = (100*(hp-10))/level
     b = -100 -(ev/4) -(2*base)
     return a+b+1
     
 				
 def getStatsfromIV(poke):
+    """ Calculates stats at given level based on IV.
+        Returns dictionary of statname->statvalue. """
     level = poke.level                
     name = poke.name
     base = dex.dex[int(nameList.name[name])]
     base = [int(i) for i in base]
     ev = poke.evL
     nat = nature.natures[poke.nature]
-    print nat
 
     new_HP  = hpstat_calc(poke.iv["HP"],base[0],ev[0],level)
-    new_Atk = stat_calc(poke.iv["Attack"],base[1],ev[1],level)#*1.1
+    new_Atk = stat_calc(poke.iv["Attack"],base[1],ev[1],level)
     new_Def = stat_calc(poke.iv["Defense"],base[2],ev[2],level)
     new_Spe = stat_calc(poke.iv["Speed"],base[5],ev[3],level)
-    new_SpA = stat_calc(poke.iv["Special Attack"],base[3],ev[4],level)#*0.9
+    new_SpA = stat_calc(poke.iv["Special Attack"],base[3],ev[4],level)
     new_SpD = stat_calc(poke.iv["Special Defense"],base[4],ev[5],level)
 
     new_Stat = [new_HP,new_Atk,new_Def,new_SpA,new_SpD,new_Spe]
@@ -80,12 +97,14 @@ def getStatsfromIV(poke):
     new_Stat[nat[1]-1] *= 1.1
     
     status = "HP, Attack, Defense, Special Attack, Special Defense, Speed"
-    statlist = status.split(',')
+    statlist = status.split(', ')
     new_Stats = dict(zip(statlist,new_Stat))
     print new_Stats
     return new_Stats
     
 def getIVfromStats(poke):
+    """ Calculates IVs based on stats.
+        Returns a dict of statname->statIV."""
     level = poke.level
     name = poke.name
     base = dex.dex[int(nameList.name[name])]
@@ -101,11 +120,13 @@ def getIVfromStats(poke):
    
     new_IV = [new_HP,new_Atk,new_Def,new_SpA,new_SpD,new_Spe]
     status = "HP, Attack, Defense, Special Attack, Special Defense, Speed"
-    statlist = status.split(',')
+    statlist = status.split(', ')
     new_IV = dict(zip(statlist,new_Stat))
     print new_IV
+    return new_IV
 
-def getHP(mon):
+def getHiddenPower(mon):
+    """ Finds the hidden power type and power. """
     a = int(mon.iv["HP"])
     b = int(mon.iv["Attack"])
     c = int(mon.iv["Defense"])
@@ -113,11 +134,11 @@ def getHP(mon):
     e = int(mon.iv["Special Attack"])
     f = int(mon.iv["Special Defense"])
 
-    HPtype = math.floor(( ((a%2) + (2*(b%2)) + (4*(c%2)) 
-                        + (8*(d%2)) + (16*(e%2)) + (32*(f%2)) )*15.0)/63.0 )
+    HPtype = math.floor((((a%2) + (2*(b%2)) + (4*(c%2)) 
+                        + (8*(d%2)) + (16*(e%2)) + (32*(f%2)))*15.0)/63.0)
   
 	
-    HPpower =  (( (((a>>1) & 1) + 2*((b>>1) & 1)
+    HPpower =  (((((a>>1) & 1) + 2*((b>>1) & 1)
                         + 4*((c>>1) & 1) + 8*((d>>1) & 1)
                         + 16*((e>>1) & 1) + 32*((f>>1) & 1))*40)
                         /63)+30
@@ -135,7 +156,8 @@ def getHP(mon):
 #Absol = Pokemon("absol", iv=[20,20,20,20,20,20], level = 50, ev=[0,0,0,0,0,0])
 # For = Pokemon("forretress", level = 43, statsL=[121,94,114*(10.0/9),72*(10.0/11),57,41])
 # ghost = Pokemon("haunter", level = 58, nature="brave",statsL=[127,69*(10.0/11),57,148,84,116*(10.0/9)], ev=[0,0,0,0,0,0])
-ghost = Pokemon("haunter", level = 58, nature="brave",iv = [12,0,0,17,27,24], ev=[0,0,0,0,0,0])
+ghost = Pokemon("haunter", level=58, nature="brave",
+                iv=[12,0,0,17,27,24], ev=[0,0,0,0,0,0])
 #getHP(Pikachu)
 #getStatsfromIV(Absol)
 #print Pikachu.stats
